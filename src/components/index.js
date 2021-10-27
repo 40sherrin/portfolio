b618eaadaf263b98eae453859872fe8264f849c0
40sherrin <fareedaskari42@gmail.com>
2021-10-28 10:41:23 +1100

    clean up

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import { createGlobalStyle } from "styled-components";
-import constructionImage from '../images/undraw_under_construction_46pa.svg'
-
-import { ImageWrapper } from './styles'
 
 const GlobalStyle = createGlobalStyle`
     body{
@@ -18,13 +15,7 @@ const Home = () => {
     return (
         <>
             <GlobalStyle />
-            <ImageWrapper>
-                <div>
-                    <img src={constructionImage} alt="construction svg" />
-                    <h1>coming soon</h1>
-                    <h1>This site is under construction</h1>
-                </div>
-            </ImageWrapper>
+           
         </>
     )
 }
