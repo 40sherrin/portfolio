ee9d340b61ed02f46ed55fce085756c5b9822d2b
40sherrin <fareedaskari42@gmail.com>
2022-02-06 17:30:57 +1100

    Router debugging and config

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Routers, Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { createGlobalStyle } from "styled-components";
 import Layout from './layout';
 import Intro from './Intro';
 import Contact from './Contact';
 import Projects from './Projects';
 import About from './About';
+import NoPage from './NoPage';
 
 
 
@@ -18,18 +19,23 @@ const GlobalStyle = createGlobalStyle`
     }
 `
 
-const Home = () => {
+const App = () => {
     return (
         <>
-            <GlobalStyle />
-            <Layout>
-                <Intro />
-                <About />
-                <Contact />
-                <Projects />
-            </Layout>
-        </>
+        <GlobalStyle />
+        <Router>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Intro />} />
+                    <Route path='contact' element={<Contact />} />
+                    <Route path='projects' element={<Projects />} />
+                    <Route path='about' element={<About />} />
+                    <Route path='*' element={<NoPage />} />
+                </Route>
+            </Routes>
+        </Router>
+    </>
     )
 }
 
-export default Home
+export default App;
