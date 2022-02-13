47e1b869c66dd6386fbc397085b103f5c5cd0a62
40sherrin <fareedaskari42@gmail.com>
2022-02-13 18:26:20 +1100

    body style and config

--- src/components/styles.js
+++ src/components/styles.js
@@ -3,11 +3,16 @@ import {styled, createGlobalStyle} from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     body{
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        font-family: sans-serif;
-        background: #111;
+        font-family: 'Poppins', sans-serif;
+        font-size: 15px;
         color: #fff;
+        font-weight: 500;
+        line-height: 1.6;
+        position: relative;
+        background-size: cover;
+        background-position: center;
+        height: 100%;
+        width: 100%;
+        background-color: #111;
     }
 `
