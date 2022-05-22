ab4f0e9b502a1b2336b073d46d62f4334295df60
40sherrin <fareedaskari42@gmail.com>
2022-05-22 10:39:57 +1000

    Working on about me page

--- src/components/About/AboutElements.js
+++ src/components/About/AboutElements.js
@@ -4,6 +4,10 @@ export const Container = styled.section`
     width: 100%;
     position: relative;
     padding: 0 20px;
+    @media (min-width: 1200px) {
+        max-width: 1140px;
+        margin: 0 auto;
+    }
 `
 export const AboutMeHeader = styled.div`
     width: 100%;
@@ -32,4 +36,37 @@ export const SpanResume = styled.span`
     font-weight: 800;
     transform: translateY(-50%);
     color: rgba(255, 255, 255, 0.07);
+`
+export const PersonalInfoContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 10px;
+`
+export const PersonalInfoWrapper = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 10px;
+`
+export const PersonalInfoH3Header = styled.h3`
+    grid-column-start: 1;
+    grid-column-end: 3;
+    font-size: 26px;
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0;
+    margin: 0;
+`
+export const PersonalInfo = styled.div`
+
+`
+export const DownloadButton = styled.a`
+    
+`
+export const PersonalInfoBoxWrapper = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 10px;
+`
+export const PersonlInfoAwardBox = styled.div`
+   
 `
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,8 +3,17 @@ import {
     AboutMeHeader,
     H1Header,
     Span,
-    SpanResume
+    SpanResume,
+    PersonalInfoContainer,
+    PersonalInfoWrapper,
+    PersonalInfoH3Header,
+    PersonalInfo,
+    DownloadButton,
+    PersonalInfoBoxWrapper,
+    PersonlInfoAwardBox
 } from './AboutElements';
+
+import resume from './../../Docs/Fareed\'s resume.docx'
 
 const About = () => {
     return (
@@ -13,6 +22,45 @@ const About = () => {
                 <H1Header>About <Span>Me</Span></H1Header>
                 <SpanResume>resume</SpanResume>
             </AboutMeHeader>
+            <PersonalInfoContainer>
+                <PersonalInfoWrapper>
+                    <PersonalInfoH3Header>personal info</PersonalInfoH3Header>
+                    <PersonalInfo>
+                        <ul>
+                            <li><span>first name :</span><span>Fareed</span></li>
+                            <li><span>last name :</span><span>Askari</span></li>
+                            <li><span>age :</span><span>24</span></li>
+                            <li><span>Freelance :</span><span>Yes</span></li>
+                        </ul>
+                    </PersonalInfo>
+                    <PersonalInfo>
+                        <ul>
+                            <li><span>Address :</span><span>Sydney, Australia</span></li>
+                            <li><span>Phone :</span><span>+61 470 594 555</span></li>
+                            <li><span>Email :</span><span>fareedaskari42@gmail.com</span></li>
+                            <li><span>Languages :</span><span>Persian/Dari, English</span></li>
+                        </ul>
+                    </PersonalInfo>
+                    <DownloadButton href={resume} download>
+                        <span>Download cv</span>
+                        <span> icon</span>
+                    </DownloadButton>
+                </PersonalInfoWrapper>
+                <PersonalInfoBoxWrapper>
+                    <PersonlInfoAwardBox>
+                        <b>5 <p>years of experience</p></b>
+                    </PersonlInfoAwardBox>
+                    <PersonlInfoAwardBox>
+                        <b>5 <p>years of experience</p></b>
+                    </PersonlInfoAwardBox>
+                    <PersonlInfoAwardBox>
+                        <b>5 <p>years of experience</p></b>
+                    </PersonlInfoAwardBox>
+                    <PersonlInfoAwardBox>
+                        <b>5 <p>years of experience</p></b>
+                    </PersonlInfoAwardBox>
+                </PersonalInfoBoxWrapper>
+            </PersonalInfoContainer>
         </Container>
     )
 }
